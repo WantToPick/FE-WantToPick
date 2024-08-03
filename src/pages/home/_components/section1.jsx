7a8febb9efb1b7325fe8b2c86ef615fd27493902
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-08-03 22:53:22 +0900

    section4 완료

--- src/pages/home/_components/section1.jsx
+++ src/pages/home/_components/section1.jsx
@@ -15,7 +15,7 @@ export default function Section1() {
                     <span className='text-2xl font-bold text-[#FFC4C7] ml-2'>PICK'</span>
                 </div>
                 <div className='flex items-center justify-center mb-5'>
-                    <img src={bannerImage} alt='WTP' className='w-28 h-28 mr-4' />
+                    <img src={bannerImage} alt='WTP' className='w-44 h-44 mr-4' />
                     <div className='text-left text-xl'>
                         <p className='font-medium'>국내유일 엔터테인먼트 X 연습생 매칭 웹 서비스</p>
                         <p className='text-gray-500'>나는 너를 픽하길 원해: 원투픽</p>
--- src/pages/home/_components/section4.jsx
+++ src/pages/home/_components/section4.jsx
@@ -1,7 +1,27 @@
+import React from 'react';
+import shieldIcon from '../../../assets/images/home/security.png'; // 방패 아이콘 이미지 경로
+import logoImage from '../../../assets/images/logo.png'; // 로고 이미지 경로
+
 export default function Section4() {
-    return(
-        <div>
-            Section4
-        </div>
+    return (
+        <section className="py-32 px-44 bg-gradient-to-b from-[#FFEAEB] to-white mt-10">
+            <div className="container mx-auto flex items-center">
+                <div className="w-1/2 relative">
+                    <img src={shieldIcon} alt="Shield Icon"/>
+                </div>
+                <div className="w-1/2 pl-16">
+                    <h2 className="text-4xl font-bold mb-4">Picker’s 엔터테인먼트 기업 <br></br> 완전인증</h2>
+                    <p className="mb-4">
+                        원투픽만의 완전인증 시스템을 통해 엔터테인먼트 전문성과 신뢰도를 높이고 <br />
+                        Pick과 Picker가 더욱 안심하고 매칭을 진행할 수 있어요. <br />
+                        안전한 서비스를 위한 노력들도 계속 되고요.
+                    </p>
+                    <p>
+                        이 외에도 연습생과 엔터의 관계를 안전하게 보호할 수 있는<br />
+                        서비스를 지속적으로 준비하고 있어요.
+                    </p>
+                </div>
+            </div>
+        </section>
     );
 }
