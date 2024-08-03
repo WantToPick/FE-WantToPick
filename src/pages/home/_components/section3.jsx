a60c85f70ac05c02160907dbf4f231170f515f55
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-08-03 21:04:47 +0900

    section3 수정

--- src/pages/home/_components/section3.jsx
+++ src/pages/home/_components/section3.jsx
@@ -38,10 +38,19 @@ export default function Section3() {
                             </div>
                         </div>
                         <h3 className="text-xl font-bold text-center mb-4">전문가에게 피드백 받기</h3>
-                        <div className="text-left">
-                            <p className="bg-blue-100 rounded-lg p-4 mb-2">조금 더 낮은 음악대의 노래가 좋을 거 같아요 .</p>
-                            <p className="bg-pink-100 rounded-lg p-4 mb-2">춤 연습을 조금 더 하면 실력을 늘 것 같네요 !</p>
-                            <p className="bg-gray-100 rounded-lg p-4">충분한 끼가 있고 소질이 있네요 :)</p>
+                        <div className="text-left space-y-2">
+                            <div className="relative bg-blue-100 rounded-lg p-4 mb-2">
+                                <p>조금 더 낮은 음악대의 노래가 좋을 거 같아요.</p>
+                                <div className="absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-full w-0 h-0 border-t-8 border-t-transparent border-b-8 border-b-transparent border-r-8 border-r-blue-100"></div>
+                            </div>
+                            <div className="relative bg-pink-100 rounded-lg p-4 mb-2">
+                                <p>춤 연습을 조금 더 하면 실력을 늘 것 같네요!</p>
+                                <div className="absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-full w-0 h-0 border-t-8 border-t-transparent border-b-8 border-b-transparent border-r-8 border-r-pink-100"></div>
+                            </div>
+                            <div className="relative bg-gray-100 rounded-lg p-4">
+                                <p>충분한 끼가 있고 소질이 있네요 :)</p>
+                                <div className="absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-full w-0 h-0 border-t-8 border-t-transparent border-b-8 border-b-transparent border-r-8 border-r-gray-100"></div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -53,8 +62,8 @@ export default function Section3() {
                             </div>
                         </div>
                         <h3 className="text-xl font-bold text-center mb-4">소속사에서 나를 픽 !</h3>
-                        <div className="flex items-start justify-start h-32 rounded-md mb-2">
-                            <img src={wtpLogo} alt="WTP Logo" className="w-24" />
+                        <div className="flex items-center justify-center h-32 mb-2">
+                            <img src={wtpLogo} alt="WTP Logo" className="w-28" />
                         </div>
                         <p className="text-center text-gray-500">우리 회사랑 너무 잘 맞을 거 같아요 !</p>
                     </div>
