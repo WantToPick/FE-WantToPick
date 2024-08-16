315c5e978a34aa7699e80101ab96fbc6a15fc689
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-08-16 15:19:56 +0900

    포트폴리오페이지 구조 설정

--- src/pages/portfolio/page.jsx
+++ src/pages/portfolio/page.jsx
@@ -1,7 +1,18 @@
-export default function PortfolioPage(){
-    return(
-        <div>
-            포트폴리오 페이지
+import React from 'react';
+import Sidebar from './_components/sideBar';
+import ProfileHeader from './_components/profileHeader';
+import { Outlet } from 'react-router-dom';
+
+export default function PortfolioPage() {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <div className="flex flex-col flex-grow">
+        <ProfileHeader />
+        <div className="flex-grow p-6">
+          <Outlet />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
