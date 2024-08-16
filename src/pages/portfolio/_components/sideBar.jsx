141a12197389c6e7e51025b4a2d5ff4f8496c43c
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-08-16 16:28:14 +0900

    사이드바 디자인

--- src/pages/portfolio/_components/sideBar.jsx
+++ src/pages/portfolio/_components/sideBar.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../../assets/images/logo.png';
-
-// React Icons 임포트
 import { FaHome, FaFileAlt, FaRegImage } from 'react-icons/fa';
 import { MdVideoLibrary } from 'react-icons/md';
+import { routes } from '../../../constants/routes'
 
 export default function Sidebar() {
   return (
@@ -17,13 +16,13 @@ export default function Sidebar() {
 
       <ul className="space-y-4">
         <li className="border-b border-gray-300">
-          <NavLink to="/home" className="flex items-center space-x-2 text-black hover:font-bold">
+          <NavLink to="/" className="flex items-center space-x-2 text-black hover:font-bold">
             <FaHome className="w-5 h-5 text-gray-400" />
             <span>메인 화면으로 돌아가기</span>
           </NavLink>
         </li>
         <li className="border-b border-gray-300">
-          <NavLink to="/profile" className="flex items-center space-x-2 text-black hover:font-bold">
+          <NavLink to={routes.portfolio} activeClassName="font-bold" className="flex items-center space-x-2 text-black hover:font-bold">
             <FaFileAlt className="w-5 h-5 text-gray-400" />
             <span>자기소개서</span>
           </NavLink>
@@ -35,13 +34,13 @@ export default function Sidebar() {
           </div>
           <ul className="ml-7 space-y-2 my-2">
             <li>
-              <NavLink to="/portfolio/video/vocal" activeClassName="font-bold" className="text-black hover:font-bold">보컬</NavLink>
+              <NavLink to="/portfolio/vocal" activeClassName="font-bold" className="text-black hover:font-bold">보컬</NavLink>
             </li>
             <li>
-              <NavLink to="/portfolio/video/dance" activeClassName="font-bold" className="text-black hover:font-bold">댄스</NavLink>
+              <NavLink to="/portfolio/dance" activeClassName="font-bold" className="text-black hover:font-bold">댄스</NavLink>
             </li>
             <li>
-              <NavLink to="/portfolio/video/rap" activeClassName="font-bold" className="text-black hover:font-bold">랩</NavLink>
+              <NavLink to="/portfolio/rap" activeClassName="font-bold" className="text-black hover:font-bold">랩</NavLink>
             </li>
           </ul>
         </li>
