a5dd49c3a01868d6512fac86b85abccab32af8fc
iinuyha <122349402+iinuyha@users.noreply.github.com>
2024-08-17 14:14:07 +0900

    포트폴리오페이지 사이드바 height 조정

--- src/pages/portfolio/page.jsx
+++ src/pages/portfolio/page.jsx
@@ -5,11 +5,11 @@ import { Outlet } from 'react-router-dom';
 
 export default function PortfolioPage() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen overflow-hidden">
       <Sidebar />
-      <div className="flex flex-col flex-grow">
+      <div className="flex flex-col flex-grow h-screen">
         <ProfileHeader />
-        <div className="flex-grow p-6">
+        <div className="flex-grow overflow-y-auto p-6">
           <Outlet />
         </div>
       </div>
